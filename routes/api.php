2afdfd1b6fb6836f82a899a264e73b8063606cca
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2025-01-23 14:34:49 +0700

    er

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\OtpController;
 use App\Http\Controllers\Auth\ApiBaseController;
-Route::match(['post','get','delete'],'login',[ApiBaseController::class,'login'])->name('login');
+Route::match(['post','get','delete'],'login',[ApiBaseController::class,'login']);
 Route::match(['post','get','delete'],'register',[ApiBaseController::class,'register']);
 Route::match(['post','get'],'wa-otp/generate',[OtpController::class,'generate']);
 Route::match(['post','get'],'wa-otp/validate',[OtpController::class,'validate']);
