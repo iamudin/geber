1f074519bb716bc78b96f67b268fd880fe2ca126
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2025-02-10 15:32:20 +0700

    update

--- app/Http/Controllers/Api/SupplierController.php
+++ app/Http/Controllers/Api/SupplierController.php
@@ -25,13 +25,12 @@ function store(Request $request){
         'nib'=>['required','string'],
         'type'=>['required','in:distributor,produsen'],
     ]);
-    $supplier = $data->load('supplier.files');
-    // return $supplier;
-    // $supplier = $data->supplier;
-    return $supplier->supplier->foto_display_produk;
     if(!$data->isSupplier()){
         $supplier = $data->supplier()->create($validator);
+    }else{
+        $supplier = $data->supplier;
     }
+
     if($request->foto_depan_tempat_usaha){
        $supplier->addFile([
         'file'=>$request->file('foto_depan_tempat_usaha'),
@@ -39,11 +38,19 @@ function store(Request $request){
         'mime_type'=>['image/png','image/jpeg']
        ]);
     }
-    if($request->foto_lokasi_produksi){
-        $supplier->addMediaFromRequest('foto_lokasi_produksi')->toMediaCollection('foto_lokasi_produksi');
+    if($request->hasFile('foto_lokasi_produksi')){
+        $supplier->addFile([
+            'file'=>$request->file('foto_lokasi_produksi'),
+            'purpose'=>'foto_lokasi_produksi',
+            'mime_type'=>['image/png','image/jpeg']
+           ]);
     }
-    if($request->foto_gudang){
-        $supplier->addMediaFromRequest('foto_gudang')->toMediaCollection('foto_gudang');
+    if($request->hasFile('foto_gudang')){
+        $supplier->addFile([
+            'file'=>$request->file('foto_gudang'),
+            'purpose'=>'foto_gudang',
+            'mime_type'=>['image/png','image/jpeg']
+           ]);
     }
     if($request->foto_display_produk){
         foreach($request->foto_display_produk as $key=>$foto){
@@ -53,8 +60,6 @@ function store(Request $request){
                 'mime_type'=>['image/png','image/jpeg']
                ]);
         }
-
-
     }
     return response()->json(['success'=>'Supplier berhasil dibuat.'],200);
 }
