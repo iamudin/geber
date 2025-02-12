c2949663bd935a5893794dcb329e1c46ab04c9db
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2025-02-12 14:51:56 +0700

    sdfdsf

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Supplier extends Model
 {
     use HasFactory,Fileable;
-    protected $fillable = ['name','description','address','type','nib'];
+    protected $fillable = ['name','description','address','type','nib','status'];
 
     public function user(){
         return $this->belongsTo(User::class);
